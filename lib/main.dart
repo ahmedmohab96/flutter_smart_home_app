5e3cd3213701e8bd03470f5775eda780b6dae0cd
ahmedmohab96 <ahmdmhabhbyb@gmail.com>
2022-02-19 01:08:51 +0200

    https://github.com/ahmedmohab96/flutter_smart_home_app.git

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+// ignore_for_file: import_of_legacy_library_into_null_safe
+
+import 'package:flutter/material.dart';
+import 'package:flutter_smart_home_app/scopedModel/connectedModel.dart';
+import 'package:scoped_model/scoped_model.dart';
+import 'pages/home_page_body.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  final ApplianceModel model = ApplianceModel();
+
+  @override
+  Widget build(BuildContext context) {
+    return ScopedModel<ApplianceModel>(
+      model: model,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: MyHomePage(model),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage(this.model);
+
+  final ApplianceModel model;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+      color: Color(0xfffcfcfd),
+      child: HomePageBody(widget.model),
+    ));
+  }
+}
